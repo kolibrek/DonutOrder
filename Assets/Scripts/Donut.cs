7dd3038824d6d8e48bc99f47e194e4f3bab15da7
Kevin Clemons <10484164@CS518-03.ad.uvu.edu>
2016-03-10 09:55:21 -0700

    Initial Commit

---
+++ Assets/Scripts/Donut.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Donut : ScriptableObject {
+
+	float price;
+	Shape shape;
+	Dough dough;
+	Glaze glaze;
+	List<Toppings> toppings;
+
+	public Donut() {
+		price = 1.00f;
+		shape = Shape.Loop;
+		dough = Dough.Original;
+		glaze = Glaze.None;
+		toppings = new List<Toppings> ();
+	}
+
+	public void SetShape(Shape shapeType) {
+		shape = shapeType;
+	}
+
+	public void SetDough(Dough doughType) {
+		dough = doughType;
+	}
+
+	public void SetGlaze(Glaze glazeType) {
+		glaze = glazeType;
+	}
+
+	public void AddTopping(Toppings toppingType) {
+		toppings.Add (toppingType);
+	}
+
+	public void RemoveTopping(Toppings toppingType) {
+		toppings.Remove (toppingType);
+	}
+
+	public override string ToString() {
+		string info = "Donut: \n";
+		info += shape.ToString () + '\n';
+		info += dough.ToString () + '\n';
+		info += glaze.ToString () + '\n';
+		for (int i = 0; i < toppings.Count; i++) {
+			info += toppings[i].ToString () + '\n';
+		}
+		info += "Price: " + price.ToString("c2");
+
+		return info;
+	}
+}
+
+public enum Shape {
+	Loop,
+	Bar,
+	Filled
+}
+
+public enum Dough {
+	Original,
+	SourCream,
+	Chocolate,
+	Cake
+}
+
+public enum Glaze {
+	None,
+	Glaze,
+	Maple,
+	Powdered,
+	Chocolate,
+	StrawberryIce
+}
+
+public enum Toppings {
+	None,
+	Sprinkles,
+	Drizzle,
+	ChocolateChip,
+	CookieCrumble
+}
---
+++ Assets/Scripts/DonutScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DonutScript : MonoBehaviour {
+
+	List<Donut> donuts;
+	public Text donutInfo;
+
+	// Use this for initialization
+	void Start () {
+		donutInfo = GameObject.Find ("DonutInfo").GetComponent<Text> ();
+		donuts = new List<Donut> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (donuts.Count > 0) {
+			Display();
+		}
+	}
+
+	public void Display() {
+		string donutList = "";
+		for (int i = 0; i < donuts.Count; i++) {
+			donutList += "[\n";
+			donutList += donuts[i].ToString();
+			donutList += "\n]";
+		}
+		donutInfo.text = donutList;
+	}
+
+	public void AddDonut() {
+		Donut new_donut = ScriptableObject.CreateInstance<Donut>();
+		donuts.Add (new_donut);
+	}
+}
