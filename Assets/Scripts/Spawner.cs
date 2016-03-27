9bfb3baa6e0cbac29584802deffc4aaa05610087
Kevin Clemons <kolibermedia@gmail.com>
2016-03-27 11:58:07 -0600

    Spawner

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+	public GameObject spawnObject;
+	GameObject clone;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Spawn() {
+		clone = Instantiate<GameObject>(spawnObject);
+		clone.transform.SetParent(this.transform.parent);
+		clone.transform.position = Input.mousePosition;
+	}
+}
